4a031543a7d5f5484cb2f3d1bda39757110b00fd
Grupo Dallax <grupodallax@gmail.com>
2026-03-21 01:56:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8048658367-6132c",
+  "appId": "1:904958259874:web:9e111834ea842101b55dfb",
+  "apiKey": "AIzaSyBb0bgizUoixkspo6lphedOAHNs0ct0nck",
+  "authDomain": "studio-8048658367-6132c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "904958259874"
+};
